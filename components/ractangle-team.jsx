4bc33c0a156d13ad07a0f4ccffad3ac504063364
Aditya Rizki Pratama <arizki717@gmail.com>
2023-03-22 19:04:58 +0700

    update: slot distribution

--- components/ractangle-team.jsx
+++ components/ractangle-team.jsx
@@ -1,12 +1,22 @@
-const RactangleTeam = ({name, image= '', className=''}) => {
+const RactangleTeam = ({ name, image = '', className = '' }) => {
   return (
-    <div className={`flex items-center gap-2 ${className}`}>
-      <div className="w-8 h-8 bg-white rounded-full">
-        <img src={image} alt={name} className="w-full h-full object-cover" />
+    <div className={`relative flex flex-col items-center gap-2 ${className}`}>
+      <svg viewBox='0 0 100 100' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+        <polygon
+          className="relative h-[123px]"
+          fill='transparent'
+          stroke='#fff'
+          strokeWidth='1'
+          height="80px"
+          points='50 1 95 25 95 75 50 99 5 75 5 25' />
+      </svg>
+      <div className='absolute flex flex-col items-center top-4'>
+        <div className='w-full h-full '>
+          <img src={image} alt={name} loading="lazy" className='w-full h-full object-contain inset-0' />
+        </div>
       </div>
-      <p className="text-white text-sm">{name}</p>
     </div>
   );
-}
+};
 
-export default RactangleTeam
+export default RactangleTeam;
--- sections/home/slot-distribution.jsx
+++ sections/home/slot-distribution.jsx
@@ -7,9 +7,16 @@ const SlotDistribution = () => {
         className='text-accent'>SLOT</span> DISTRIBUTION</h2>
       <div className='grid sm:grid-cols-4 grid-cols-1 mt-5'>
         <div className='col-span-2'>
-          <h3 className='text-xl text-white'>PARTNER TEAMS</h3>
-          <div className='flex'>
-            <RactangleTeam name="ad" />
+          <h3 className='sm:text-2xl text-xl text-white text-center py-3 '>PARTNER TEAMS</h3>
+          <div className='flex flex-wrap gap-2'>
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/1.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/2.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/3.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/4.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/5.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/6.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/7.png" />
+            <RactangleTeam className="w-2/12" name="" image="https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/partners-bfs/8.png" />
           </div>
         </div>
       </div>
