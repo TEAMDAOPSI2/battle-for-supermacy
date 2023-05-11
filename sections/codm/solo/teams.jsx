50fcb36a0dfc98dbbaf9c50116039e79c5e56ff5
Aditya Rizki Pratama <arizki717@gmail.com>
2023-05-11 21:23:11 +0700

    xxxx

--- sections/codm/solo/teams.jsx
+++ sections/codm/solo/teams.jsx
@@ -1,52 +1,61 @@
-import { Tab } from '@headlessui/react';
-import ConferencePanel from '@/sections/home/conference-panel';
-import GroupstagePanel from '@/sections/home/groupstage-panel';
-import TabTitle from '@/components/tab-title';
-import { useContext } from 'react';
-import articleCtx from '@/context/article-ctx';
-import PlayoffPanel from '@/sections/home/playoff-panel';
+import ErrImg from '@/public/images/hex-err.png';
 
 const Team = ({ team }) => {
-  const img = `https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/hex-flags/${team.code3}.png`
-  console.log(team);
+  const img = `https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/hex-flags/${team.code3}.png`;
   return (
-    <div className='flex flex-col items-center font-inter gap-6 relative'>
-      {/*<svg viewBox='0 0 100 100' version='1.1' xmlns='http://www.w3.org/2000/svg'>*/}
-      {/*  /!*stroke gradient*!/*/}
-      {/*  <defs>*/}
-      {/*    <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">*/}
-      {/*      <stop offset="0%" stopColor="#0070f3" />*/}
-      {/*      <stop offset="100%" stopColor="#db2b42" />*/}
-      {/*    </linearGradient>*/}
-      {/*  </defs>*/}
-
-      {/*  <polygon*/}
-      {/*    className="relative h-[123px]"*/}
-      {/*    stroke={'url(#gradient)'}*/}
-      {/*    fill='transparent'*/}
-      {/*    strokeWidth='1'*/}
-      {/*    height="80px"*/}
-      {/*    points='50 1 95 25 95 75 50 99 5 75 5 25' />*/}
-      {/*</svg>*/}
-
-      {/*<div className='absolute w-10/12 h-full flex flex-col justify-center items-center'>*/}
-      {/*  <div className='flag w-[40px] text-3xl mb-1 font-flag text-center'>*/}
-      {/*    { team?.code }*/}
-      {/*  </div>*/}
-
-      {/*  <div className='text'>*/}
-      {/*    <p className='text-white text-center uppercase font-medium font-inter text-sm'>{team?.name}</p>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      <img src={img} alt={team.code} />
-
+    <div className='flex items-center sm:w-2/12 w-4/12 '>
+      <img
+        src={img}
+        alt={team.code}
+        className='w-full h-full'
+        onError={(e) => {
+          e.target.src = ErrImg.src;
+        }}
+      />
     </div>
   );
 };
 
+const TeamList = ({ teams, numItemsPerRow }) => {
+  const totalRows = Math.ceil(teams.length / numItemsPerRow);
+  let teamIndex = 0;
+  let rows = [];
 
-const Teams = ({data}) => {
+  for (let i = 1; i <= totalRows; i++) {
+    const rowItems = [];
+
+    if (i % 2 === 0) {
+      // even row
+      for (let j = 0; j < numItemsPerRow - 1; j++) {
+        if (teamIndex < teams.length) {
+          rowItems.push(<Team team={teams[teamIndex]} />);
+          teamIndex++;
+        } else {
+          // add empty data to fill row
+          rowItems.push(<div />);
+        }
+      }
+    } else {
+      // odd row
+      const itemsInRow = Math.min(numItemsPerRow, teams.length - teamIndex + 1);
+      for (let j = 0; j < itemsInRow; j++) {
+        if (teamIndex < teams.length) {
+          rowItems.push(<Team team={teams[teamIndex]} />);
+          teamIndex++;
+        } else {
+          // add empty data to fill row
+          rowItems.push(<div className={`w-${12/numItemsPerRow}/12`} />);
+        }
+      }
+    }
+
+    rows.push(<div className='flex flex-row flex-wrap justify-center'>{rowItems}</div>);
+  }
+
+  return <div className='wrapper-teams-list-hex mx-auto'>{rows}</div>;
+};
+
+const Teams = ({ data }) => {
   const { participants: teams } = data;
   console.log(teams);
   return (
@@ -62,13 +71,15 @@ const Teams = ({data}) => {
         <div className='card bg-soft-black px-6 my-6 py-3'>
           <div className='flex flex-col mb-6 border border-dotted border-white sm:px-6 px-1 py-3'>
             <h3 className='text-2xl font-bold text-white mb-6'>Country Participants</h3>
-            <div className={`grid sm:grid-cols-6 grid-cols-3 gap-2`}>
-              {
-                teams.map((team, index) => (
-                  <Team key={index} team={team} />
-                ))
-              }
+
+            <div className="sm:block hidden">
+              <TeamList teams={teams} numItemsPerRow={6} />
             </div>
+
+            <div className="sm:hidden block">
+              <TeamList teams={teams} numItemsPerRow={3} />
+            </div>
+
           </div>
         </div>
 
