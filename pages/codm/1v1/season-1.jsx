ee77e32d3eb08caabbc6e3dec2dc93650a51f2b3
Aditya Rizki Pratama <arizki717@gmail.com>
2023-06-01 13:23:48 +0700

    FF

--- pages/codm/1v1/season-1.jsx
+++ pages/codm/1v1/season-1.jsx
@@ -36,7 +36,7 @@ const Season1 = () => {
 
   const Layout = () => {
     const data = {
-      banner: 'banner/5v5-s3.mp4',
+      banner: 'banner/1v1-mp-s1.mp4',
       format: [
         '3 Rounds - TPP',
         '1 Kill = 1 Point',
