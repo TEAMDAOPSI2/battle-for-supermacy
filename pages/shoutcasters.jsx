cc08ab7f3a3ac4504d4f1cb521c4fbccc1a9140d
Aditya Rizki Pratama <arizki717@gmail.com>
2023-07-04 12:02:09 +0700

    FFF

--- pages/shoutcasters.jsx
+++ pages/shoutcasters.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import ErrImg from '@/public/images/hex-err.png';
+import ErrImg from '@/public/images/NINJA.png';
 
 const casters = [
   {
@@ -10,8 +10,72 @@ const casters = [
     name: '1185',
     image: 'https://via.placeholder.com/150',
   },
+  {
+    name: 'NINJA',
+  },
+  {
+    name: '1227',
+  },
+  {
+    name: 'NINJA C',
+  },
+  {
+    name: 'NINJA',
+  },
+  {
+    name: 'NINJA',
+  },
 ];
 
+const GenPyramid = ({ global_ranks }) => {
+  const totalItems = 8;
+  const totalRows = 3;
+  const pyramid = [];
+  let currentItem = 0;
+  // row 1 = 1
+  // row 2 = 2
+  // row 3 = 3
+  // row 4 = 4
+  // row 5 = 5
+  // generate pyramid with 12 items and 5 rows
+  // Calculate the number of items in each row of the pyramid
+  const itemsPerRow = [];
+  let remainingItems = totalItems;
+  for (let i = 1; i <= totalRows; i++) {
+    const items = Math.min(i, remainingItems);
+    itemsPerRow.push(items);
+    remainingItems -= items;
+  }
+
+  // Generate the pyramid rows
+  let index = 0;
+  for (let row = 0; row < totalRows; row++) {
+    const items = [];
+    for (let col = 0; col < itemsPerRow[row]; col++) {
+      if (currentItem < totalItems) {
+        const team = global_ranks[currentItem];
+        items.push(
+          <Team team={team} type='casters' />,
+        );
+        currentItem++;
+        index++;
+      }
+    }
+    pyramid.push(
+      <div key={row}
+           className={`flex  ${row === (5 - 1) ? 'sm:justify-between justify-evenly' : 'justify-center'} ${row > 0 && 'sm:-mt-10 -mt-3'}`}>
+        {items}
+      </div>,
+    );
+  }
+
+  // Render the pyramid container
+  return (
+    <div className='pyramid-container flex flex-col w-full sm:max-w-full m-auto'>
+      {pyramid}
+    </div>
+  );
+};
 const TeamList = ({ teams, numItemsPerRow, type }) => {
   // const totalRows = Math.ceil(teams.length / numItemsPerRow);
   const totalRows = Math.ceil(teams.length / numItemsPerRow) + Math.ceil((teams.length % numItemsPerRow) / 2);
@@ -53,25 +117,20 @@ const TeamList = ({ teams, numItemsPerRow, type }) => {
   return <div className='wrapper-teams-list-hex mx-auto'>{rows}</div>;
 };
 const Team = ({ team, type }) => {
-  const img = `https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/BFS/gamemasters/${team.name}.png`;
+  const img = team?.name ? `https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/BFS/gamemasters/${team?.name}.png` : ErrImg;
 
   return (
-    <div className='flex flex-col relative items-center sm:w-2/12 w-3/12 '>
-      {
-        team.name !== '' ? (
-          <>
-            <img
-              src={img}
-              alt={team.name}
-              className='w-full h-full px-2'
-              loading={'lazy'}
-              onError={(e) => {
-                e.target.src = ErrImg.src;
-              }}
-            />
-          </>
-        ) : (<div className='w-full h-full block' />)
-      }
+    <div className='relative'>
+      <img className='sm:w-[190px] w-[120px] object-cover px-1 sm:mb-3 mb-0'
+           src={img}
+           onError={(e) => {
+             e.target.onerror = null;
+             e.target.src = ErrImg;
+           }}
+           alt='' />
+      {/*<div className="absolute top-0 right-0 mr-10 mt-3 rounded-full bg-primary w-[30px] h-[30px] flex items-center justify-center">*/}
+      {/*  {rank}*/}
+      {/*</div>*/}
     </div>
   );
 };
@@ -88,7 +147,8 @@ function Shoutcasters() {
             <h2 className='text-[3.3rem] font-bold font-inter text-center'>Shoutcasters</h2>
           </div>
           <div className='flex flex-col items-center justify-center min-h-[500px] max-w-[1020px] mx-auto'>
-            <TeamList teams={casters} numItemsPerRow={2} type='shoutcasters' />
+            {/*<TeamList teams={casters} numItemsPerRow={2} type='shoutcasters' />*/}
+            <GenPyramid global_ranks={casters} />
           </div>
         </div>
       </section>
