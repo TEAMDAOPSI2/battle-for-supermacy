a889f707bbc82ed6d851f0ba0f3b52ab9cdf6bf7
Aditya Rizki Pratama <arizki717@gmail.com>
2023-07-04 11:27:36 +0700

    FF

--- pages/shoutcasters.jsx
+++ pages/shoutcasters.jsx
@@ -56,14 +56,14 @@ const Team = ({ team, type }) => {
   const img = `https://raw.githubusercontent.com/teamdao-psi3/esport-team/main/BFS/gamemasters/${team.name}.png`;
 
   return (
-    <div className='flex flex-col relative items-center sm:w-3/12 w-4/12 px-2'>
+    <div className='flex flex-col relative items-center sm:w-2/12 w-3/12 '>
       {
         team.name !== '' ? (
           <>
             <img
               src={img}
               alt={team.name}
-              className='w-full h-full'
+              className='w-full h-full px-2'
               loading={'lazy'}
               onError={(e) => {
                 e.target.src = ErrImg.src;
@@ -85,10 +85,10 @@ function Shoutcasters() {
       <section className='default-section'>
         <div className='px-2'>
           <div className='page-section-title'>
-            <h2 className='font-bold font-inter text-center'>Shoutcasters</h2>
+            <h2 className='text-[3.3rem] font-bold font-inter text-center'>Shoutcasters</h2>
           </div>
           <div className='flex flex-col items-center justify-center min-h-[500px] max-w-[1020px] mx-auto'>
-            <TeamList teams={casters} numItemsPerRow={4} type='shoutcasters' />
+            <TeamList teams={casters} numItemsPerRow={2} type='shoutcasters' />
           </div>
         </div>
       </section>
